b3e9dedfd03fbeb02d24004f85a07513c2f4328c
Сидоров Владимир Александрович <Sidorov.V.Ale@sberbank.ru>
2021-11-20 20:47:21 +0300

    Добавлена обработка ошибок с недоступностью сервиса.

--- project/faucet/views.py
+++ project/faucet/views.py
@@ -1,7 +1,10 @@
+from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import CreateView
 
 from faucet.models import FaucetRequests
+
+from django.apps import apps
 
 
 class FaucetRequestsView(SuccessMessageMixin, CreateView):
@@ -11,9 +14,19 @@ class FaucetRequestsView(SuccessMessageMixin, CreateView):
     fields = ['address', 'value']
 
     def get_context_data(self, **kwargs):
+        web3 = apps.get_app_config('faucet').web3
+        if web3 == 'Fail':
+            status = False
+        else:
+            status = web3.isConnected()
+
+        if not status:
+            messages.error(self.request, 'Sorry, service unavailable!')
+
         context = super().get_context_data(**kwargs)
         req = FaucetRequests.objects.filter(state=False)
         context['request_list'] = req
         context['queue_size'] = len(req)
+        context['service_status'] = status
 
         return context
